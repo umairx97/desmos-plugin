0512c0f4ab3d6bb862ec6d81ad79c6c30c3786b8
umairx97 <umair.bajwa@hotmail.com>
2020-05-07 22:38:58 +0500

    feat: add Desmos graph calculator

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Desmos from 'desmos'
+import axios from 'axios'
 
 import connectToDatoCms from './connectToDatoCms';
 import './style.sass';
@@ -10,16 +13,55 @@ import './style.sass';
 }))
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      expressions: []
+    }
+
+    this.renderDesmos = this.renderDesmos.bind(this)
+
+  }
   static propTypes = {
     fieldValue: PropTypes.bool.isRequired,
   }
 
+  renderDesmos() {
+    const { plugin } = this.props
+    const self = this
+    var elt = document.getElementById('desmos-calculator');
+    const calculator = Desmos.GraphingCalculator(elt)
+    calculator.setExpression({ id: 'graph1', latex: 'y=x^2' })
+    self.setState({ expressions: calculator.getState().expressions.list })
+    calculator.updateSettings({
+      invertedColors: true,
+      fontSize: 12,
+      backgroundColor: '#4f515a'
+    });
+    calculator.observeEvent('change', function () {
+      self.setState({ expressions: calculator.getState().expressions.list })
+    })
+  }
+
+  componentDidMount() {
+    this.renderDesmos()
+  }
+
   render() {
     const { fieldValue } = this.props;
-
+    console.log("EXP IN STATE", this.state.expressions)
     return (
       <div className="container">
-        {JSON.stringify(fieldValue)}
+        {fieldValue}
+        <input
+          type="text"
+          name="desmos"
+          placeholder="Paste Desmos Url"
+          // value={expressions}
+          onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
+        />
+        <br />
+        <div id="desmos-calculator" style={{ height: '500px' }}/>
       </div>
     );
   }
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,19 @@
+/* eslint-disable */
 import React from 'react';
 import { render } from 'react-dom';
 
 import Main from './Main';
+
+(function() {
+  const desmosEmbed = document.createElement('script')
+  desmosEmbed.src = 'https://www.desmos.com/api/v1.5/calculator.js?apiKey=dcb31709b452b1cf9dc26972add0fda6'
+  document.body.append(desmosEmbed)
+})()
 
 window.DatoCmsPlugin.init((plugin) => {
   plugin.startAutoResizer();
 
   const container = document.createElement('div');
   document.body.appendChild(container);
-
   render(<Main plugin={plugin} />, container);
 });
